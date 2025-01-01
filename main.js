bf13a553a29be33d138effe3e2bf9d53e83b505b
Durjoy kundu <durjoykundu07@gmail.com>
2025-01-01 23:25:07 +0600

    some basics of node js

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// import {a,b,c,d,e} from "./mymodule.js"
+// console.log(a,b,c,d,e)
+
+
+
+// import harry from "./mymodule.js"
+// // I can import default properties in any name.
+// console.log(harry);
+
+const c = require("./mymodule2.js")
+console.log(c);
+
+
+// // const { createServer } = require('node:http');
+
+// // const fs = require('fs');
+// import { createServer } from "http";
+
+
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
